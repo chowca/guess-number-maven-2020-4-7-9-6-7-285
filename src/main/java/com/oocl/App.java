0c61ecf8169860d5f328602ab3f57e3540367532
chowca <56864611+chowca@users.noreply.github.com>
2020-04-08 23:04:08 +0800

    [Cavin Chow] passed the test should_verify_input_integrity

--- src/main/java/com/oocl/App.java
+++ src/main/java/com/oocl/App.java
@@ -3,8 +3,9 @@ package com.oocl;
 import java.util.ArrayList;
 import java.util.List;
 
-public class App
-{
+public class App {
+    public static final String ERROR_MSG = "ERROR: Wrong Input, Input again";
+    public static final int INPUT_LENGTH_LIMIT = 4;
     private String answer;
     private List<String> guessNumbers = new ArrayList<String>();
 
@@ -30,14 +31,28 @@ public class App
         String result = "";
         int a = 0;
         int b = 0;
-        for(int inputIndex = 0; inputIndex < input.length(); inputIndex++){
-            if(answer.indexOf(input.charAt(inputIndex)) == inputIndex) {
+        for (int inputIndex = 0; inputIndex < input.length(); inputIndex++) {
+            if (answer.indexOf(input.charAt(inputIndex)) == inputIndex) {
                 a++;
-            }
-            else if(answer.indexOf(input.charAt(inputIndex)) != -1) {
+            } else if (answer.indexOf(input.charAt(inputIndex)) != -1) {
                 b++;
             }
         }
-        return result += String.format("%dA%dB",a,b);
+        return result += String.format("%dA%dB", a, b);
+    }
+
+    public String verifyInput(String input) {
+        String eorrorMsg = "PASS";
+        if (input.length() != INPUT_LENGTH_LIMIT) {
+            eorrorMsg = ERROR_MSG;
+        } else {
+            for (int inputIndex = 0; inputIndex < input.length(); inputIndex++) {
+                if((input.lastIndexOf(input.charAt(inputIndex)) != inputIndex) || (!Character.isDigit(input.charAt(inputIndex)))){
+                    eorrorMsg = ERROR_MSG;
+                    break;
+                }
+            }
+        }
+        return eorrorMsg;
     }
 }
--- src/test/java/com/oocl/AppTest.java
+++ src/test/java/com/oocl/AppTest.java
@@ -14,20 +14,19 @@ public class AppTest {
         App game = new App();
         String answer = "1234";
         String input = "1234";
-        String result = "";
         //when
         game.setAnswer(answer);
         game.addGuessNumbers(input);
         //then
         assertThat(game.check(input), is("4A0B"));
     }
+
     @Test
     public void should_return_for_totally_wrong_answer() {
         //given
         App game = new App();
         String answer = "1234";
         String input = "0987";
-        String result = "";
         //when
         game.setAnswer(answer);
         game.addGuessNumbers(input);
@@ -40,7 +39,6 @@ public class AppTest {
         App game = new App();
         String answer = "1234";
         String input = "1289";
-        String result = "";
         //when
         game.setAnswer(answer);
         game.addGuessNumbers(input);
@@ -54,7 +52,6 @@ public class AppTest {
         App game = new App();
         String answer = "1234";
         String input = "4321";
-        String result = "";
         //when
         game.setAnswer(answer);
         game.addGuessNumbers(input);
@@ -68,11 +65,28 @@ public class AppTest {
         App game = new App();
         String answer = "1234";
         String input = "1243";
-        String result = "";
         //when
         game.setAnswer(answer);
         game.addGuessNumbers(input);
         //then
         assertThat(game.check(input), is("2A2B"));
+    }
+
+    @Test
+    public void should_verify_input_integrity() {
+        //given
+        App game = new App();
+        String inputNormal = "1234";
+        String inputLessDigit = "12";
+        String inputExtraDigit = "12345";
+        String inputDuplicateDigit = "1134";
+        String inputNotdigit = "1$a\n";
+        //when
+        //then
+        assertThat(game.verifyInput(inputNormal), is("PASS"));
+        assertThat(game.verifyInput(inputLessDigit), is("ERROR: Wrong Input, Input again"));
+        assertThat(game.verifyInput(inputExtraDigit), is("ERROR: Wrong Input, Input again"));
+        assertThat(game.verifyInput(inputDuplicateDigit), is("ERROR: Wrong Input, Input again"));
+        assertThat(game.verifyInput(inputNotdigit), is("ERROR: Wrong Input, Input again"));
     }
 }
