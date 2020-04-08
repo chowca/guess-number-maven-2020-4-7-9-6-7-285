cfca4b0781be0a604c82724664e599153e2ae748
chowca <56864611+chowca@users.noreply.github.com>
2020-04-08 21:48:22 +0800

    [Calvin Chow] passed the test should_return_for_exact_answer()

--- src/main/java/com/oocl/App.java
+++ src/main/java/com/oocl/App.java
@@ -1,13 +1,40 @@
 package com.oocl;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.util.ArrayList;
+import java.util.List;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private String answer;
+    private List<String> guessNumbers = new ArrayList<String>();
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public List<String> getInputs() {
+        guessNumbers = new ArrayList<String>();
+        guessNumbers = this.guessNumbers;
+        return guessNumbers;
+    }
+
+    public void addGuessNumbers(String guessNumber) {
+        this.guessNumbers.add(guessNumber);
+    }
+
+    public String check(String input) {
+        String result = "";
+        int a = 0;
+        int b = 0;
+        for(int inputIndex = 0; inputIndex < input.length(); inputIndex++){
+            if(answer.indexOf(input.charAt(inputIndex)) == inputIndex) {
+                a++;
+            }
+        }
+        return result += String.format("%dA%dB",a,b);
     }
 }
--- src/test/java/com/oocl/AppTest.java
+++ src/test/java/com/oocl/AppTest.java
@@ -2,9 +2,23 @@ package com.oocl;
 
 import org.junit.Test;
 
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class AppTest {
     @Test
-    public void helloWorld() {
-
+    public void should_return_for_exact_answer() {
+        //given
+        App game = new App();
+        String answer = "1234";
+        String input = "1234";
+        String result = "";
+        //when
+        game.setAnswer(answer);
+        game.addGuessNumbers(input);
+        assertThat(game.check(input), is("4A0B"));
     }
+
 }
