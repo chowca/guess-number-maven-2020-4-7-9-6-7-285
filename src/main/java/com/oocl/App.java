87b9605f3352b830dcb39fa3653d398147be2cb3
chowca <56864611+chowca@users.noreply.github.com>
2020-04-14 20:10:04 +0800

    [Calvin Chow] format code

--- src/main/java/com/oocl/App.java
+++ src/main/java/com/oocl/App.java
@@ -9,7 +9,7 @@ public class App {
         RandomAnswerGenerator answerGenerator = new RandomAnswerGenerator();
         GuessNumber game = new GuessNumber(answerGenerator.generate());
         String inputString = "";
-        String inputCheckResult = "";
+        String inputCheckResult;
 
         System.out.println(String.format(GameMsg.WELCOME_MSG, Config.INPUT_LENGTH_LIMIT));
         System.out.println(String.format(GameMsg.CHANCES_AND_INPUT_MSG, game.getGameChanges(), Config.INPUT_LENGTH_LIMIT));
@@ -25,7 +25,7 @@ public class App {
             game.minusGameChanges();
             if ((!game.check(inputString).equals(String.format(Config.WINNING_PATTERN, Config.INPUT_LENGTH_LIMIT))) && (game.getGameChanges() > 0)) {
                 System.out.println(String.format("%s\n", game.check(inputString)));
-                System.out.println(String.format(GameMsg.CHANCES_AND_INPUT_MSG,game.getGameChanges(), Config.INPUT_LENGTH_LIMIT));
+                System.out.println(String.format(GameMsg.CHANCES_AND_INPUT_MSG, game.getGameChanges(), Config.INPUT_LENGTH_LIMIT));
             } else if (game.check(inputString).equals(String.format(Config.WINNING_PATTERN, Config.INPUT_LENGTH_LIMIT))) {
                 System.out.println(GameMsg.WIN_MSG);
                 break;
