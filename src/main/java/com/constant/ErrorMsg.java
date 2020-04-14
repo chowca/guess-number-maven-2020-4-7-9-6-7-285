2db988bd6d65b9da2df04ec3ebee8b894009c04b
chowca <56864611+chowca@users.noreply.github.com>
2020-04-14 19:57:58 +0800

    [Calvin Chow] extracted method, revised logic in "verifyInput"

--- src/main/java/com/constant/ErrorMsg.java
+++ src/main/java/com/constant/ErrorMsg.java
@@ -4,4 +4,5 @@ public class ErrorMsg {
     public static final String PASS_MSG = "PASS";
     public static final String INPUT_LENGTH_NOT_MATCH_ERROR_MSG = "ERROR: Wrong Input, Input again";
     public static final String INPUT_IS_NOT_DIGIT_ERROR_MSG = "ERROR: Wrong Input, Input again";
+    public static final String INPUT_DUPLICATE_DIGIT_ERROR_MSG = "ERROR: Wrong Input, Input again";
 }
--- src/main/java/com/oocl/ConsoleInputReader.java
+++ src/main/java/com/oocl/ConsoleInputReader.java
@@ -15,16 +15,26 @@ public class ConsoleInputReader implements InputReader {
 
     public String verifyInput(String input) {
         String errorMsg = ErrorMsg.PASS_MSG;
-        if (input.length() != Config.INPUT_LENGTH_LIMIT) {
+        if (isInputLengthMatchAnswerLength(input)) {
             errorMsg = ErrorMsg.INPUT_LENGTH_NOT_MATCH_ERROR_MSG;
         } else {
             for (int inputIndex = 0; inputIndex < input.length(); inputIndex++) {
-                if ((input.lastIndexOf(input.charAt(inputIndex)) != inputIndex) || (!Character.isDigit(input.charAt(inputIndex)))) {
+                //check for not digit error
+                if (!Character.isDigit(input.charAt(inputIndex))) {
                     errorMsg = ErrorMsg.INPUT_IS_NOT_DIGIT_ERROR_MSG;
+                    break;
+                }
+                //check for duplicate digit error
+                if ((input.lastIndexOf(input.charAt(inputIndex)) != inputIndex)) {
+                    errorMsg = ErrorMsg.INPUT_DUPLICATE_DIGIT_ERROR_MSG;
                     break;
                 }
             }
         }
         return errorMsg;
+    }
+
+    private boolean isInputLengthMatchAnswerLength(String input) {
+        return input.length() != Config.INPUT_LENGTH_LIMIT;
     }
 }
